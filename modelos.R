551320ae44aa7839068db9155bc86015f768a8c2
Marcson <azevedomarcson@id.uff.br>
2023-12-12 02:13:45 -0300

    criando codigo de modelagem

---
+++ modelos.R
@@ -0,0 +1,24 @@
+# modelagem
+# 
+ # EPA trabalho final PPEPA
+
+require(PNADcIBGE)
+require(survey)
+require(dplyr)
+
+diretorio <- "C:/ence/PPEPA2023/epa_final/"
+
+# carregar versao
+
+pnadc_design <- readRDS(file = paste0(diretorio,"dados/pnadc_design.rds"))
+
+require(emdi)
+
+fit <- fh(fixed = )
+
+
+
+
+
+
+
